31e6a1440d2c035ec141e51cfa088ae867fe1467
Ellarh <ellamoves01@gmail.com>
2022-05-22 12:22:04 +0100

    Add project files.

---
+++ password generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Workingwithfiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (int i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + rand.Next(0, 26));
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+
+        }  
+    }
+}
+
+
